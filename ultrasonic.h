4d9e4dfbfcb6e88c93974d35a5f24c8a74d8ec73
kengjit <kengjit123@gmail.com>
2021-11-11 23:29:54 +0800

    Update ultrasonic

--- ultrasonic.h
+++ ultrasonic.h
@@ -1,13 +1,15 @@
-#include "MKL25Z4.h"
-
-#define RED_LED 18 				//PortB Pin 18
-#define GREEN_LED 19 			//PortB Pin 19
-#define BLUE_LED 5 				//PortD Pin 1
+#include "MKL25Z4.h"                    // Device header
 
 #define TRIGGER 12 				//PortA Pin 12
 #define ECHO 6 						//PortD Pin 6
 
-#define MASK(x) (1 << (x)) 
+#define MASK(x) (1 << (x))
+
+uint32_t threshold = 3500;
+volatile uint32_t hasStarted = 0; 
+uint32_t stage = 0; 
+volatile uint32_t timerStart = 0;
+volatile uint32_t timerEnd = 0;
 
 void initUltraSonic(void) {
 	
@@ -23,7 +25,7 @@ void initUltraSonic(void) {
 	PORTD->PCR[ECHO] |= PORT_PCR_MUX(1);							//Configure MUX setting to GPIO
 	
 	PORTD->PCR[ECHO] |= PORT_PCR_IRQC(0x0b);					//enable PortD 6 interrupt
-	PORTD->PCR[ECHO] |= PORT_PCR_PS_MASK;							//enable pull select
+	PORTD->PCR[ECHO] &= ~PORT_PCR_PS_MASK;							//enable pull select
 	PORTD->PCR[ECHO] |= PORT_PCR_PE_MASK;							//enable pull enable
 	
 	NVIC_EnableIRQ(PORTD_IRQn);
